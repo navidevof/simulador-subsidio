c302a4630dfba8e3cdb3439e0af76c65855bcbfd
Ivan <ivan@elsociodigital.com>
2024-09-27 20:27:53 -0500

    Feature: add logo and adjust logic

--- src/helpers/format.ts
+++ src/helpers/format.ts
@@ -1,9 +1,13 @@
-export const formatCurrency = (price: number) => {
-	const newPrice = price.toString();
-	const formattedValue = parseInt(newPrice).toLocaleString("es-CO", {
+export function formatCurrency(value: number): string {
+	return new Intl.NumberFormat("es-CO", {
 		style: "currency",
 		currency: "COP",
 		minimumFractionDigits: 0,
-	});
-	return formattedValue + " COP";
-};
+		maximumFractionDigits: 0,
+	}).format(value);
+}
+
+export function parseCurrency(value: string): number {
+	const numericString = value.replace(/[^0-9.]/g, "");
+	return parseFloat(numericString) || 0;
+}
--- src/views/Simulator.vue
+++ src/views/Simulator.vue
@@ -1,27 +1,28 @@
 <template>
 	<main class="flex items-center justify-center min-h-screen h-full py-10">
-		<section
+		<form
 			class="border flex flex-col gap-y-4 rounded-md p-5 max-w-screen-sm mx-auto w-10/12"
 		>
 			<h3 class="font-bold text-2xl text-center">Simulador de subsidio</h3>
-			<small class="text-custom-primary text-center -mt-4"
-				>Ten en cuenta que esta simulación solo aplica para proyectos
+			<small class="text-custom-primary text-center -mt-4">
+				Ten en cuenta que esta simulación solo aplica para proyectos
 				<strong>VIP</strong> y <strong>VIS</strong>
 			</small>
+
 			<div class="flex flex-col gap-y-2">
 				<strong>¿Valor del inmueble?</strong>
 				<input
 					type="number"
-					min="0"
-					max="200000000"
-					placeholder="Ingresa el valor de tu inmueble"
 					class="w-full"
-					@blur="data.price > MAX_PRICE ? (data.price = MAX_PRICE) : 0"
+					@blur="data.price = data.price > MAX_PRICE ? MAX_PRICE : data.price"
+					:max="MAX_PRICE"
 					v-model="data.price"
 				/>
 			</div>
+
+			<!-- Grupo de Sisben -->
 			<div class="flex flex-col gap-y-2">
-				<strong>¿A que grupo de sisben perteneces?</strong>
+				<strong>¿A qué grupo de sisben perteneces?</strong>
 				<select v-model="data.sisben">
 					<option value="">Selecciona una opción</option>
 					<option value="group-1">A1-C8</option>
@@ -29,8 +30,10 @@
 					<option value="group-3">No tengo sisben</option>
 				</select>
 			</div>
+
+			<!-- Salario -->
 			<div class="flex flex-col gap-y-2">
-				<strong>¿Cuanto ganas?</strong>
+				<strong>¿Cuánto ganas?</strong>
 				<div class="flex flex-col gap-y-2">
 					<label class="flex items-center gap-2">
 						<input
@@ -61,29 +64,37 @@
 					</label>
 				</div>
 			</div>
+
+			<!-- Valor de la Vivienda -->
 			<div class="flex flex-col gap-y-2">
 				<h4 class="font-bold text-xl">Posible valor de tu vivienda</h4>
-				<h3 class="font-bold text-2xl">
-					{{ formatCurrency(newValue) }}
-				</h3>
+				<h3 class="font-bold text-2xl">{{ formatCurrency(newValue) }}</h3>
 			</div>
+
+			<!-- Subsidios Aplicables -->
 			<div class="flex flex-col gap-y-2">
-				<h5 class="font-bold">Aplican los subsidos de:</h5>
+				<h5 class="font-bold">Aplican los subsidios de:</h5>
 				<ul class="list-disc flex flex-col pl-6">
-					<li class="text-sm" v-show="!concurrencia && miCasaYa">
-						<strong>Mi casa ya:</strong> {{ miCasaYa }}
+					<li class="text-sm" v-if="miCasaYa > 0 && concurrencia === 0">
+						<strong>Mi casa ya:</strong> {{ formatCurrency(miCasaYa) }} o
+						{{ getSmmlv(miCasaYa) }} SMMLV
 					</li>
-					<li
-						class="text-sm"
-						v-show="!concurrencia && !miCasaYa && cajaDeCompensacion"
-					>
-						<strong>Caja de compensación:</strong> {{ cajaDeCompensacion }}
+					<li class="text-sm" v-if="cajaDeCompensacion > 0">
+						<strong>Caja de compensación:</strong>
+						{{ formatCurrency(cajaDeCompensacion) }} o
+						{{ getSmmlv(cajaDeCompensacion) }} SMMLV
 					</li>
-					<li class="text-sm" v-show="concurrencia">
-						<strong>Concurrencia:</strong> {{ concurrencia }}
+					<li class="text-sm" v-if="concurrencia > 0">
+						<strong>Concurrencia:</strong> {{ formatCurrency(concurrencia) }} o
+						{{ getSmmlv(concurrencia) }} SMMLV
+					</li>
+					<li v-if="totalDiscount === 0">
+						<strong>No hay ningún descuento</strong>
 					</li>
 				</ul>
 			</div>
+
+			<!-- Nota Informativa -->
 			<p class="text-xs text-custom-primary font-light">
 				Es importante que estos valores sean revisados y confirmados por tu
 				constructora para asegurar que los subsidios mencionados sean
@@ -91,14 +102,14 @@
 				aplican todos los subsidios disponibles y que el valor del descuento
 				puede cambiar.
 			</p>
-		</section>
+		</form>
 	</main>
 </template>
 
 <script lang="ts" setup>
+import { computed, ref } from "vue";
 import { SMMLV_MOCK, SUBSIDIES_MOCK } from "@/constants/mocks";
 import { formatCurrency } from "@/helpers/format";
-import { computed, ref } from "vue";
 
 const MAX_PRICE = 200000000;
 
@@ -109,82 +120,49 @@ const data = ref({
 });
 
 const miCasaYa = computed(() => {
-	if (data.value.sisben == "group-1") {
-		discount.value.miCasaYa = SUBSIDIES_MOCK.miCasaYa.group1 * SMMLV_MOCK;
-		return `${formatCurrency(SUBSIDIES_MOCK.miCasaYa.group1 * SMMLV_MOCK)} o ${
-			SUBSIDIES_MOCK.miCasaYa.group1
-		} SMMLV`;
+	if (data.value.sisben === "group-1") {
+		return SUBSIDIES_MOCK.miCasaYa.group1 * SMMLV_MOCK;
 	}
-
-	if (data.value.sisben == "group-2") {
-		discount.value.miCasaYa = SUBSIDIES_MOCK.miCasaYa.group2 * SMMLV_MOCK;
-		return `${formatCurrency(SUBSIDIES_MOCK.miCasaYa.group2 * SMMLV_MOCK)} o ${
-			SUBSIDIES_MOCK.miCasaYa.group2
-		} SMMLV`;
+	if (data.value.sisben === "group-2") {
+		return SUBSIDIES_MOCK.miCasaYa.group2 * SMMLV_MOCK;
 	}
-
-	discount.value.miCasaYa = 0;
-
-	return "";
+	return 0;
 });
 
 const cajaDeCompensacion = computed(() => {
 	if (
-		data.value.salary == "group-1" &&
-		(data.value.sisben == "group-3" || data.value.sisben == "")
+		data.value.salary === "group-1" &&
+		(data.value.sisben === "group-3" || data.value.sisben === "")
 	) {
-		discount.value.cajaDeCompensacion =
-			SUBSIDIES_MOCK.cajaDeCompensacion.group1 * SMMLV_MOCK;
-		return `${formatCurrency(
-			SUBSIDIES_MOCK.cajaDeCompensacion.group1 * SMMLV_MOCK
-		)} o ${SUBSIDIES_MOCK.cajaDeCompensacion.group1} SMMLV`;
+		return SUBSIDIES_MOCK.cajaDeCompensacion.group1 * SMMLV_MOCK;
 	}
-
 	if (
-		data.value.salary == "group-2" &&
-		(data.value.sisben == "group-3" || data.value.sisben == "")
+		data.value.salary === "group-2" &&
+		(data.value.sisben === "group-3" || data.value.sisben === "")
 	) {
-		discount.value.cajaDeCompensacion =
-			SUBSIDIES_MOCK.cajaDeCompensacion.group2 * SMMLV_MOCK;
-		return `${formatCurrency(
-			SUBSIDIES_MOCK.cajaDeCompensacion.group2 * SMMLV_MOCK
-		)} o ${SUBSIDIES_MOCK.cajaDeCompensacion.group2} SMMLV`;
+		return SUBSIDIES_MOCK.cajaDeCompensacion.group2 * SMMLV_MOCK;
 	}
-
-	discount.value.cajaDeCompensacion = 0;
-
-	return "";
+	return 0;
 });
 
 const concurrencia = computed(() => {
-	if (data.value.salary == "group-1" && data.value.sisben == "group-1") {
-		discount.value.concurrencia =
-			SUBSIDIES_MOCK.concurrencia.group1 * SMMLV_MOCK;
-		return `${formatCurrency(
-			SUBSIDIES_MOCK.concurrencia.group1 * SMMLV_MOCK
-		)} o ${SUBSIDIES_MOCK.concurrencia.group1} SMMLV`;
+	if (data.value.salary === "group-1" && data.value.sisben === "group-1") {
+		return SUBSIDIES_MOCK.concurrencia.group1 * SMMLV_MOCK;
 	}
-
-	discount.value.concurrencia = 0;
-
-	return "";
+	return 0;
 });
 
-const discount = ref({
-	miCasaYa: 0,
-	cajaDeCompensacion: 0,
-	concurrencia: 0,
+const totalDiscount = computed(() => {
+	return miCasaYa.value + cajaDeCompensacion.value + concurrencia.value;
 });
-
-const totalDiscount = computed(
-	() =>
-		discount.value.miCasaYa +
-		discount.value.cajaDeCompensacion +
-		discount.value.concurrencia
-);
 
 const newValue = computed(() => {
-	if (totalDiscount.value > data.value.price) return 0;
-	return data.value.price - totalDiscount.value;
+	return totalDiscount.value > data.value.price
+		? 0
+		: data.value.price - totalDiscount.value;
 });
+
+const getSmmlv = (value: number) => {
+	return (value / SMMLV_MOCK).toFixed(0);
+};
 </script>
